45d6a5a830ebcde3a84b3fcd74eceff909462e2a
amanagg639 <amanagg639@gmail.com>
2025-06-05 14:55:22 +0530

    [Aman]add. Aman Controller file

---
+++ src/main/java/com/team_test/demo/controller/AmanController.java
@@ -0,0 +1,14 @@
+package com.team_test.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class AmanController {
+    @GetMapping("/aman")
+    public String sayHello() {
+        return "Hello from Aman!";
+    }
+}
